6736e0dee9850c7ff317e8fe34cb4b37929a1d5d
Akhil Sharma <akhilsharma6721@gmail.com>
2023-05-28 15:42:42 +0530

    added authentication component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Home from './routes/home/home.component.jsx';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './routes/navigation/navigation.component.jsx';
-import SignIn from './routes/sign-in/sign-in.component.jsx';
+import Authentication from './routes/authentication/authentication.component.jsx';
 
 const Shop = () => {
   return (
@@ -17,7 +17,7 @@ const App = () => {
       <Route path='/' element={<Navbar />}>
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
-        <Route path='sign-in' element={<SignIn />} />
+        <Route path='sign-in' element={<Authentication />} />
       </Route>
     </Routes>
   );
