5bc66f3fe6494e2f3d4f2d2429bee2604562f3fd
Akhil Sharma <akhilsharma6721@gmail.com>
2023-06-07 20:17:17 +0530

    added styled components and routes

--- src/components/directory/directory.styles.jsx
+++ src/components/directory/directory.styles.jsx
@@ -2,7 +2,10 @@ import styled from 'styled-components';
 
 export const DirectoryContainer = styled.div`
   width: 100%;
+  height: 75vh;
   display: flex;
   flex-wrap: wrap;
-  justify-content: space-between;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
 `;
